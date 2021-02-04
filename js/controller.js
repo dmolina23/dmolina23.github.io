3235bc4b1328dae34609f6eb3acc88c722cf7771
dmolina23 <dmolina2311@gmail.com>
2021-02-04 10:01:51 +0100

    add menu

--- js/controller.js
+++ js/controller.js
@@ -7,11 +7,8 @@ $.controller.panel_inicial = "";
 $.controller.activate = function (panel_name) {
 
     // console.log("cambio old::"+$.controller.active_panel+" new::"+panel_name);
-
     $($.controller.active_panel).hide();
-
     $(panel_name).show();
-
     $.controller.active_panel = panel_name;
 
 };
@@ -21,19 +18,12 @@ $.controller.init = function (panel_inicial) {
     // console.log("Panel inicial="+panel_inicial);
 
     $('[id^="menu_"]').each(function () {
-
         var $this = $(this);
-
         var menu_id = $this.attr('id');
-
         var panel_id = menu_id.replace('menu_', 'panel_');
 
-
-
         $("#" + menu_id).click(function () {
-
             $.controller.activate("#" + panel_id);
-
         });
 
         // console.log("id_menu::" + menu_id + " id_panel" + panel_id);
